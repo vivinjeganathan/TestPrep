b3adcf1c46917b81afc9ecd50619a626894b41c2
Vivin <vivinjeganathan@yahoo.com>
2019-02-17 10:01:00 -0600

    first commit

---
+++ Prep1/Photo/PhotoDataHelper.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoDataHelper.swift
+//  Prep1
+//
+//  Created by Vivin Jeganathan on 2/16/19.
+//  Copyright © 2019 Vivin Jeganathan. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataHelper: NSObject {
+
+}
---
+++ Prep1/Photo/PhotoModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoModel.swift
+//  Prep1
+//
+//  Created by Vivin Jeganathan on 2/16/19.
+//  Copyright © 2019 Vivin Jeganathan. All rights reserved.
+//
+
+import UIKit
+
+class PhotoModel: NSObject {
+
+}
